6dc00b2bfee26670b650afa573640c531f97c84b
Olufemi Oyedepo <OOyedepo@wragbysolutions.com>
2019-06-07 20:09:20 +0100

    implemented authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using GeofencingWebApi.Models.WebResponse;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace GeofencingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("gettoken")]
+        public IActionResult Authenticate()
+        {
+            var authResponse = new AuthResponse();
+
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection
+                {
+                    ["grant_type"] = _configuration.GetSection("AuthConfig").GetSection("grant_type").Value,
+                    ["client_id"] = _configuration.GetSection("AuthConfig").GetSection("client_id").Value,
+                    ["client_secret"] = _configuration.GetSection("AuthConfig").GetSection("client_secret").Value,
+                    ["tenant_id"] = _configuration.GetSection("AuthConfig").GetSection("tenant_id").Value,
+                    ["resource"] = _configuration.GetSection("AuthConfig").GetSection("resource").Value
+                };
+
+                string url = _configuration.GetSection("AuthConfig").GetSection("url").Value;
+
+                var response = wb.UploadValues(url, "POST", data);
+                string responseInString = Encoding.UTF8.GetString(response);
+
+                authResponse = JsonConvert.DeserializeObject<AuthResponse>(responseInString);
+            }
+
+            return new JsonResult(authResponse);
+        }
+    }
+}
---
+++ Models/WebResponse/AuthResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeofencingWebApi.Models.WebResponse
+{
+    public class AuthResponse
+    {
+        public string Token_Type { get; set; }
+        public string Expires_In { get; set; }
+        public string Ext_Expires_In { get; set; }
+        public string Expires_On { get; set; }
+        public string Not_Before { get; set; }
+        public string Resource { get; set; }
+        public string Access_Token { get; set; }
+    }
+}
